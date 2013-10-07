70bbef9de558fdd732d9a272977666859ab8773c
rgatkowski <rafal.gatkowski@stxnext.pl>
2013-10-07 11:21:59 +0200

    pep8 fix

--- src/presence_analyzer/decorators.py
+++ src/presence_analyzer/decorators.py
@@ -14,11 +14,11 @@ class decoratorCache(object):
     data = {}
     last_time = None
 
-    def __init__(self, arg1 = 600):
+    def __init__(self, arg1=600):
         """
         Set arg1 and last time value.
         """
-        self.last_time = datetime.datetime(1970,1,1)
+        self.last_time = datetime.datetime(1970, 1, 1)
         self.arg1 = arg1
 
     def __call__(self, func):
