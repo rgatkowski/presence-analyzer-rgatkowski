beceb50dfde46aea2e8ca1a7fc41890ffc1156b9
rgatkowski <rafal.gatkowski@stxnext.pl>
2013-10-07 12:30:25 +0200

    bug #664 - apply fixes: arg1 -> sec_timeout, lock on function, seconds -> total_seconds()

--- src/presence_analyzer/decorators.py
+++ src/presence_analyzer/decorators.py
@@ -14,21 +14,22 @@ class decoratorCache(object):
     data = {}
     last_time = None
 
-    def __init__(self, arg1=600):
+    def __init__(self, sec_timeout=600):
         """
         Set arg1 and last time value.
         """
         self.last_time = datetime.datetime(1970, 1, 1)
-        self.arg1 = arg1
+        self.sec_timeout = sec_timeout
 
     def __call__(self, func):
         """
         Execute function.
         """
-        def wrapped_f(*args):
-            now = datetime.datetime.now()
-            with Lock():
-                if (now - self.last_time).seconds > self.arg1:
+        with Lock():
+            def wrapped_f(*args):
+                now = datetime.datetime.now()
+
+                if (now - self.last_time).total_seconds() > self.sec_timeout:
                     self.last_time = datetime.datetime.now()
                     self.data = func()
                 return self.data
